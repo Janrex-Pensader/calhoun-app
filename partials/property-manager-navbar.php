6002aa25fe204bb17282907311fdc27977fec012
Viel Eclarinal <eclarinalviel@gmail.com>
2024-05-31 23:06:40 +0800

    Update property-manager-navbar.php

--- partials/property-manager-navbar.php
+++ partials/property-manager-navbar.php
@@ -1,3 +1,6 @@
+<!-- Hidden DP -->
+
+
 <div class="row px-12" style="background-color: #2A2F42 !important">
   <a class="navbar-brand" href="./"><img src="../img/logo.png" height=45 class="mr-3"> Maryland Evictions Online</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
@@ -16,25 +19,35 @@
     </li>
   </ul>
 </div>
-<div class="row px-12">
-  <div class="collapse navbar-collapse" id="navbar">
-    <ul class="navbar-nav">
-      <li id="nav-owners" class="nav-item">
-          <a class="nav-link" href="./owners.php?cbResetParam=1"><i class="fas fa-users"></i> Owners</a>
-      </li>
-      <li id="nav-properties" class="nav-item">
-        <a class="nav-link" href="./properties.php?cbResetParam=1"><i class="fas fa-home"></i> Properties</a>
-      </li>
-      <li id="nav-tenants" class="nav-item">
-        <a class="nav-link" href="./tenants.php?cbResetParam=1"><i class="fas fa-users"></i> Tenants</a>
-      </li>
-      <li id="nav-tenants" class="nav-item">
-        <a class="nav-link" href="./cases.php?cbResetParam=1"><i class="fas fa-file"></i> Cases</a>
-      </li>
-    </ul>
-  </div>
 
-  <div id="btn-submit-all" class="text-right hide-this mt-1">
+<div class="row px-12">
+    <div class="collapse navbar-collapse" id="navbar">
+      <ul class="navbar-nav">
+        <li id="nav-dashboard" class="nav-item">
+          <a class="nav-link" href="./index.php?cbResetParam=1"><i class="fas fa-address-card"></i> Dashboard</a>
+        </li>
+        <!-- <li class="nav-item dropdown d-none" id="nav-manage-contacts">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-address-card"></i> Manage Contacts
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="./manage-contacts.html?cbResetParam=1">All Contacts</a>
+            <a class="dropdown-item" href="./add-contact.html">Add New Contact</a>
+          </div>
+        </li> -->
+        <li id="nav-properties" class="nav-item">
+          <a class="nav-link" href="./properties.php?cbResetParam=1"><i class="fas fa-home"></i> Properties</a>
+        </li>
+        <li id="nav-tenants" class="nav-item">
+          <a class="nav-link" href="./tenants.php?cbResetParam=1"><i class="fas fa-users"></i> Tenants</a>
+        </li>
+        <li id="nav-cases" class="nav-item">
+          <a class="nav-link" href="./cases.php?cbResetParam=1&Case_Status_ID=1"><i class="fas fa-file"></i> Cases</a>
+        </li>
+      </ul>
+    </div>
+
+    <div id="btn-submit-all" class="text-right hide-this mt-1">
       <script type="text/javascript" src="https://c1abn983.caspio.com/dp/ded080003041f19ec520437ca017/emb"></script>
     </div>
     
